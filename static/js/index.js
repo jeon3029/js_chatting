81f2933adfccf037c0c2debd4f5eeaa30d89e8b6
jeon3029 <jeon3029@naver.com>
2021-01-28 13:33:50 +0900

    add timer with warning, alert colors

--- static/js/index.js
+++ static/js/index.js
@@ -58,11 +58,30 @@ socket.on('update', function(data) {
   chat.appendChild(message)
   chat.scrollTop = chat.scrollHeight;
 })
+
 const TIME_LIMIT = 20;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
-
+const FULL_DASH_ARRAY=283;
+// Warning occurs at 10s
+const WARNING_THRESHOLD = 10;
+// Alert occurs at 5s
+const ALERT_THRESHOLD = 5;
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+let remainingPathColor = COLOR_CODES.info.color;
 function startTimer() {
   timerInterval = setInterval(() => {
     // The amount of time passed increments by one
@@ -70,8 +89,11 @@ function startTimer() {
     timeLeft = TIME_LIMIT - timePassed;
     // The time left label is updated
     document.getElementById("base-timer-label").innerHTML = formatTimeLeft(timeLeft);
+    setCircleDasharray();
+    setRemainingPathColor(timeLeft);
   }, 1000);
 }
+
 function lastword_start(){
   console.log('last start')
   var last = document.getElementById('lastword')
@@ -87,6 +109,17 @@ function lastword_start(){
     <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
       <g class="base-timer__circle">
         <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+          <path
+            id="base-timer-path-remaining"
+            stroke-dasharray="283"
+            class="base-timer__path-remaining ${remainingPathColor}"
+            d="
+              M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0
+            "
+          ></path>
       </g>
     </svg>
     <span id="base-timer-label" class="base-timer__label">
@@ -111,13 +144,47 @@ function formatTimeLeft(time) {
   // The output in MM:SS format
   return `${minutes}:${seconds}`;
 }
+function calculateTimeFraction() {
+  const rawTimeFraction = timeLeft / TIME_LIMIT;
+  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+}
+// Update the dasharray value as time passes, starting with 283
+function setCircleDasharray() {
+  const circleDasharray = `${(
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
+  document
+    .getElementById("base-timer-path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
+}
+function setRemainingPathColor(timeLeft) {
+  console.log("set remain")
+  const { alert, warning, info } = COLOR_CODES;
+  // If the remaining time is less than or equal to 5, remove the "warning" class and apply the "alert" class.
+  if (timeLeft <= alert.threshold) {
+//    console.log('warning');
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(warning.color);
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(alert.color);
 
+  // If the remaining time is less than or equal to 10, remove the base color and apply the "warning" class.
+  } else if (timeLeft <= warning.threshold) {
+//    console.log('alert');
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.remove(info.color);
+    
+    document
+      .getElementById("base-timer-path-remaining")
+      .classList.add(warning.color);
+  }
+}
 function lastword_end(){
   console.log('last start')
 }
-
-
-
 /* 메시지 전송 함수 */
 function send() {
   // 입력되어있는 데이터 가져오기
